81e24424ea601791eb2c5bc4d49492b898226171
mpereirasalgado <mpereirasalgado@eniac07.danielcastelao.org>
2014-10-20 09:38:57 +0200

    refactorizar crear metodo entradaDato

--- src/refactorizar/Refactorizar.java
+++ src/refactorizar/Refactorizar.java
@@ -1,65 +1,89 @@
 package refactorizar;
 
+import java.util.Scanner;
+
 /**
  * Lista los numeros primos de dos cifras
  *
  */
-public class Refactorizar {
-
-    public static void main(String[] args) {
+public class Refactorizar 
+{
+    public static void main(String[] args) 
+    {
         boolean p = false;
         int longitud = 0;
         int contador_digitos = 0;
-        longitud = 2;
-        if (longitud <= 0) {
+        longitud = entradaDato();
+        
+        if (longitud <= 0) 
+        {
             System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
-        for (int i = 1; i <= 99999; i++) {
+        for (int i = 1; i <= 99999; i++) 
+        {
             int divisionEntera = i;
 
             int contador = 0;
 
-            while (divisionEntera != 0) {
+            while (divisionEntera != 0) 
+            {
                 divisionEntera = divisionEntera / 10;
                 contador++;
             }
             contador_digitos = contador;
 
-            if (contador_digitos == longitud) {
-                if (i < 4) {
+            if (contador_digitos == longitud) 
+            {
+                if (i < 4) 
+                {
                     p = true;
                 } else {
-                    if (i % 2 == 0) {
+                    if (i % 2 == 0) 
+                    {
                         p = false;
                     } else {
                         int contador1 = 0;
                         int i1 = 1;
                         int limite = (i - 1) / 2;
-                        if (limite % 2 == 0) {
+                        if (limite % 2 == 0) 
+                        {
                             limite--;
                         }
 
-                        while (i1 <= limite) {
-                            if (i % i1 == 0) {
+                        while (i1 <= limite) 
+                        {
+                            if (i % i1 == 0) 
+                            {
                                 contador1++;
                             }
                             i1 += 2;
-                            if (contador1 == 2) {
+                            if (contador1 == 2) 
+                            {
                                 i1 = limite + 1;
                             }
                         }
 
-                        if (contador1 == 1) {
+                        if (contador1 == 1) 
+                        {
                             p = true;
                         }
                     }
                 }
 
-                if (p == true) {
+                if (p == true) 
+                {
                     System.out.println(i);
                 }
             }
         }
+    }
+
+    private static int entradaDato() {
+        int longitud;
+        System.out.println("Numero de digitos deseados : ");
+        Scanner dato = new Scanner(System.in);
+        longitud = dato.nextInt();
+        return longitud;
     }
 
 }
